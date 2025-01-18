10aed4cfc0a720887a70e39b40f11071a3b5a2d1
Han Gao <gaohan@iscas.ac.cn>
2025-01-18 11:41:57 +0800

    unset MESA_LOADER_DRIVER_OVERRIDE if it's zink
    From: https://github.com/Icenowy/xf86-video-pvrdri/commit/c911738a01048050fed9f080912fd2a730777a74
    
    Signed-off-by: Icenowy Zheng <uwu@icenowy.me>
    Signed-off-by: Han Gao <gaohan@iscas.ac.cn>

--- hw/xfree86/drivers/modesetting/driver.c
+++ hw/xfree86/drivers/modesetting/driver.c
@@ -38,6 +38,7 @@
 
 #include <unistd.h>
 #include <fcntl.h>
+#include <stdlib.h>
 #include "xf86.h"
 #include "xf86Priv.h"
 #include "xf86_OSproc.h"
@@ -173,12 +174,22 @@ static void *
 Setup(void *module, void *opts, int *errmaj, int *errmin)
 {
     static Bool setupDone = 0;
+    char *mesa_drv;
 
     /* This module should be loaded only once, but check to be sure.
      */
     if (!setupDone) {
         setupDone = 1;
         xf86AddDriver(&modesetting, module, HaveDriverFuncs);
+
+        mesa_drv = getenv("MESA_LOADER_DRIVER_OVERRIDE");
+        if (mesa_drv && !strcmp(mesa_drv, "zink")) {
+            /*
+             * Running glamor with a forced Zink on PVR isn't economic.
+             */
+            unsetenv("MESA_LOADER_DRIVER_OVERRIDE");
+            xf86DrvMsg(-1, X_INFO, "Unsetting MESA_LOADER_DRIVER_OVERRIDE\n");
+        }
 
         /*
          * The return value must be non-NULL on success even though there
